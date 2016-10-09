8d2923a84dc1bf6dff71db69b059dfc9ecfca1c8
Sean Genabe <seangenabe@outlook.com>
2016-10-09 11:26:21 +0800

    tests

--- packages/starry._async-operation/test.js
+++
@@ -1,7 +0,0 @@
-const _setTypes = require('.')
-const t = require('ava')
-
-t(t => {
-  t.truthy(_setTypes.has(Set))
-  t.truthy(_setTypes.has(WeakSet))
-})
--- packages/starry.filter/test.js
+++ packages/starry.filter/test.js
@@ -1,7 +1,22 @@
-const _setTypes = require('.')
+const filter = require('.')
 const t = require('ava')
 
-t(t => {
-  t.truthy(_setTypes.has(Set))
-  t.truthy(_setTypes.has(WeakSet))
+t('empty', t => {
+  t.is(filter([]).length, 0)
+})
+
+function* nums() {
+  for (let i = 0; i < 10; i++) {
+    yield i
+  }
+}
+
+t('even only', t => {
+  let result = [...filter(nums(), n => n % 2 === 0)]
+  t.deepEqual(result, [0, 2, 4, 6, 8])
+})
+
+t('default predicate', t => {
+  let result = filter([false, true, false, true])
+  t.deepEqual(result, [true, true])
 })
