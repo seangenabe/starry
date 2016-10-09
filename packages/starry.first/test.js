58517bf74ac00e1886642119d18b832154818c06
Sean Genabe <seangenabe@outlook.com>
2016-10-09 21:15:28 +0800

    first: removed invalid test cases

--- packages/starry.first/test.js
+++ packages/starry.first/test.js
@@ -1,18 +1,8 @@
 const first = require('.')
 const t = require('ava')
 
-function* nums() {
-  yield 1
-  yield 2
-  yield 3
-}
-
 t(t => {
-  let result = first(nums())
-  t.is(result, 1)
+  //let result = first(nums())
+  //t.is(result, 1)
   t.is(first([4, 5]), 4)
-})
-
-t(t => {
-  t.is(first(), undefined)
 })
