f683947793bb340c52bf8861f88963f59a868a4e
Sean Genabe <seangenabe@outlook.com>
2016-10-09 21:08:46 +0800

    intersection: fix yielding

--- packages/starry.intersection/index.js
+++ packages/starry.intersection/index.js
@@ -5,9 +5,9 @@ const every = require('starry.every')
 module.exports = function* intersection(...iterables) {
   if (!iterables.length) { return }
   let first = iterables.shift()
-  return filter(first, element1 => {
+  yield* filter(first, element1 => {
     return every(iterables, iterable => {
-      return includes(iterable, first)
+      return includes(iterable, element1)
     })
   })
 }
