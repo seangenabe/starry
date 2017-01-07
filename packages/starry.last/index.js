1c5de5aa82ed13ce2d5597f57bc3213eb9fb39a2
Sean Genabe <seangenabe@outlook.com>
2017-01-08 05:32:31 +0800

    last: added tests

--- packages/starry.last/index.js
+++ packages/starry.last/index.js
@@ -3,7 +3,9 @@ const arrayTypes = require('starry._array-types')
 module.exports = function last(iterable = []) {
   if (arrayTypes.has(iterable.constructor)) {
     let len = iterable.length
-    if (len === 0) { return undefined }
+    if (len === 0) {
+      return undefined
+    }
     return iterable[iterable.length - 1]
   }
   let item
--- packages/starry.last/test.js
+++ packages/starry.last/test.js
@@ -1,15 +1,19 @@
 const last = require('.')
 const t = require('ava')
 
-t(t => {
+t('last value of an iterator', t => {
   let result = last((function*() {
-    yield 1
-    yield 2
-    yield 3
+    yield 5
+    yield 6
+    yield 7
   })())
-  t.is(result, 3)
+  t.is(result, 7)
 })
 
-t(t => {
+t('last value of an array', t => {
   t.is(last(Uint32Array.from([1, 2, 3])), 3)
+})
+
+t('last value of an empty array', t => {
+  t.is(last(Uint32Array.from([])), undefined)
 })
