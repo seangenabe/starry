95f81d631751d0f82e740054f94c664f3449d983
Sean Genabe <seangenabe@outlook.com>
2017-01-08 09:46:53 +0800

    generator-to-iterable: add module

---
+++ packages/starry.generator-to-iterable/test.js
@@ -0,0 +1,32 @@
+const test = require('ava')
+const generatorToIterable = require('.')
+
+test("environment test -- generators won't save state", t => {
+  let res = wholes()
+  let iterator1 = res[Symbol.iterator]()
+  t.deepEqual(iterator1.next(), { done: false, value: 1 })
+  t.deepEqual(iterator1.next(), { done: false, value: 2 })
+  t.deepEqual(iterator1.next(), { done: false, value: 3 })
+  let iterator2 = res[Symbol.iterator]()
+  t.deepEqual(iterator2.next(), { done: false, value: 4 })
+  t.deepEqual(iterator2.next(), { done: false, value: 5 })
+  t.deepEqual(iterator2.next(), { done: false, value: 6 })
+})
+
+test('should save original iterable state', t => {
+  let res = generatorToIterable(wholes)
+  let iterator1 = res[Symbol.iterator]()
+  t.deepEqual(iterator1.next(), { done: false, value: 1 })
+  t.deepEqual(iterator1.next(), { done: false, value: 2 })
+  t.deepEqual(iterator1.next(), { done: false, value: 3 })
+  let iterator2 = res[Symbol.iterator]()
+  t.deepEqual(iterator2.next(), { done: false, value: 1 })
+  t.deepEqual(iterator2.next(), { done: false, value: 2 })
+  t.deepEqual(iterator2.next(), { done: false, value: 3 })
+})
+
+function* wholes() {
+  for (let i = 1; i < 100; i++) {
+    yield i
+  }
+}
