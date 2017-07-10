590a4e2d70dcb24f277bd2567c13c886d4e87eb7
Sean Genabe <seangenabe@outlook.com>
2017-07-10 19:53:31 +0800

    update tests

--- packages/starry.intersection/test.js
+++ packages/starry.intersection/test.js
@@ -2,10 +2,12 @@ const intersection = require('.')
 const t = require('ava')
 
 t(t => {
-  let r1 = [...intersection([1, 2, 3], [2, 3, 4])]
+  const r1 = [...intersection([1, 2, 3], [2, 3, 4])]
   t.deepEqual(r1, [2, 3])
-  let r2 = [...intersection([2, 'a', 'c'], [], ['d', 3, 'p'])]
+  const r2 = [...intersection([2, 'a', 'c'], [], ['d', 3, 'p'])]
   t.deepEqual(r2, [])
-  let r3 = [...intersection([])]
+  const r3 = [...intersection([])]
   t.deepEqual(r3, [])
+  const r4 = [...intersection()]
+  t.deepEqual(r4, [])
 })
--- packages/starry.zip/index.d.ts
+++ packages/starry.zip/index.d.ts
@@ -1,4 +1,5 @@
 declare function zip<T1>(iterable: Iterable<T1>): Iterable<[T1]>;
 declare function zip<T1, T2>(i1: Iterable<T1>, i2: Iterable<T2>): Iterable<[T1 | undefined, T2 | undefined]>;
 declare function zip<T1, T2, T3>(i1: Iterable<T1>, i2: Iterable<T2>, i3: Iterable<T3>): Iterable<[T1 | undefined, T2 | undefined, T3 | undefined]>;
+declare function zip<TAll>(...iterables: Iterable<TAll>[]): Iterable<(TAll | undefined)[]>;
 export = zip;
