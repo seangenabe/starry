d84ccf75b9101d44f636264dee46bda0ad48e14f
Sean Genabe <seangenabe@outlook.com>
2017-07-11 15:46:28 +0800

    intersection: emit unique values

--- packages/starry.intersection/index.ts
+++ packages/starry.intersection/index.ts
@@ -2,6 +2,8 @@ import every = require('starry.every')
 import filter = require('starry.filter')
 import generatorToIterable = require('starry.generator-to-iterable')
 import includes = require('starry.includes')
+import arrayTypes = require('starry._array-types')
+import { ArrayLikeConstructor } from 'starry._array-types'
 
 function intersection<T>(
   ...iterables: Iterable<T>[]
@@ -10,7 +12,14 @@ function intersection<T>(
   if (!iterables.length) {
     return []
   }
-  let first = iterables.shift() as Iterable<T>
+  let first = new Set(iterables.shift() as Iterable<T>)
+  iterables = iterables.map(iterable => {
+    const C = iterable.constructor
+    if (arrayTypes.has(C as ArrayLikeConstructor) || C === Set) {
+      return new Set(iterable)
+    }
+    return iterable
+  })
   return generatorToIterable(function* intersectionGenerator() {
     yield* filter(first, element1 => {
       return every(iterables, iterable => {
--- packages/starry.intersection/test.js
+++ packages/starry.intersection/test.js
@@ -2,7 +2,7 @@ const intersection = require('.')
 const t = require('ava')
 
 t(t => {
-  const r1 = [...intersection([1, 2, 3], [2, 3, 4])]
+  const r1 = [...intersection(nums(), [2, 3, 4])]
   t.deepEqual(r1, [2, 3])
   const r2 = [...intersection([2, 'a', 'c'], [], ['d', 3, 'p'])]
   t.deepEqual(r2, [])
@@ -10,4 +10,12 @@ t(t => {
   t.deepEqual(r3, [])
   const r4 = [...intersection()]
   t.deepEqual(r4, [])
+  const r5 = [...intersection([2, 2], [2, 2, 2])]
+  t.deepEqual(r5, [2])
 })
+
+function* nums() {
+  yield 1
+  yield 2
+  yield 3
+}
