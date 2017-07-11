54ead371e5784076435485f61fd061119b59a859
Sean Genabe <seangenabe@outlook.com>
2017-07-12 07:05:40 +0800

    intersection: compress in test 1 :)

--- packages/starry.intersection/test.js
+++ packages/starry.intersection/test.js
@@ -2,7 +2,7 @@ const intersection = require('.')
 const t = require('ava')
 
 t(t => {
-  const r1 = [...intersection(nums(), [2, 3, 4])]
+  const r1 = [...intersection(nums(), [2, 3, 4], new Set([2, 3, 4]), nums())]
   t.deepEqual(r1, [2, 3])
   const r2 = [...intersection([2, 'a', 'c'], [], ['d', 3, 'p'])]
   t.deepEqual(r2, [])
@@ -12,8 +12,6 @@ t(t => {
   t.deepEqual(r4, [])
   const r5 = [...intersection([2, 2], [2, 2, 2])]
   t.deepEqual(r5, [2])
-  const r6 = [...intersection(new Set([1, 2]), new Set([2, 2, 3]))]
-  t.deepEqual(r6, [2])
 })
 
 function* nums() {
