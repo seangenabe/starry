d6da3ff797604d5e6732a513267ae8f1c4b2c820
Sean Genabe <seangenabe@outlook.com>
2018-05-31 02:30:28 +0800

    remove definition file from version control

--- packages/starry.reduce/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare function reduce<T = any>(iterable: Iterable<T>, accumulator: (previousValue: T, currentValue: T) => T): T;
-declare function reduce<T = any, U = any>(iterable: Iterable<T>, accumulator: (previousValue: U, currentValue: T) => U, initialValue: U): U;
-export = reduce;
