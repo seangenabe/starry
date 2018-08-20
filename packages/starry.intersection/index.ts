d724218e1e242eb9d632197d5f518533be924b4d
Sean Genabe <seanmarviolivergenabe@gmail.com>
2018-08-20 14:04:32 +0800

    starry.intersection: remove dep _arrayTypes

--- packages/starry.intersection/index.ts
+++ packages/starry.intersection/index.ts
@@ -2,8 +2,6 @@ import { every } from 'starry.every'
 import { filter } from 'starry.filter'
 import { generatorToIterable } from 'starry.generator-to-iterable'
 import { includes } from 'starry.includes'
-import { arrayTypes } from 'starry._array-types'
-import { ArrayLikeConstructor } from 'starry._array-types'
 
 export function intersection<T = any>(
   ...iterables: Iterable<T>[]
@@ -12,13 +10,6 @@ export function intersection<T = any>(
     return []
   }
   let first = new Set(iterables.shift() as Iterable<T>)
-  iterables = iterables.map(iterable => {
-    const C = iterable.constructor
-    if (arrayTypes.has(C as ArrayLikeConstructor) || C === Set) {
-      return new Set(iterable)
-    }
-    return iterable
-  })
   return generatorToIterable(function* intersectionGenerator() {
     yield* filter(first, element1 => {
       return every(iterables, iterable => {
