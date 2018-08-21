c66e05b03b0819ee4b25fd72390662e5d23693e7
Sean Genabe <seanmarviolivergenabe@gmail.com>
2018-08-21 18:20:20 +0800

    skip: add iterable test

--- packages/starry.skip/test.ts
+++ packages/starry.skip/test.ts
@@ -12,6 +12,16 @@ test('skip more than length', t => t.deepEqual(skipArr(s, 30), []))
 test('array slice', t => t.deepEqual(skipArr(['a', 'b', 'c'], 2), ['c']))
 test('typed array', t =>
   t.deepEqual(skipArr(new Uint8Array([1, 2, 3]), 1), [2, 3]))
+test('iterable', t =>
+  t.deepEqual(
+    skipArr(
+      (function*() {
+        yield* [1, 2, 3]
+      })(),
+      2
+    ),
+    [3]
+  ))
 
 test('throws error on non-finite count values', t => {
   t.throws(() => [...skip([1, 2], -Infinity as any)])
